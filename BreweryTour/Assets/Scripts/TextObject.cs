3b6727f06f8cf75bf9ce6663b3461c3e9eb93f2d
Smthy <Alexsmithgamedev@gmail.com>
2022-05-12 15:52:26 +0100

    Uopdated ignore

--- BreweryTour/Assets/Scripts/TextObject.cs
+++ BreweryTour/Assets/Scripts/TextObject.cs
@@ -1,23 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TextObject : MonoBehaviour
 {
     public GameObject[] textObjects;
 
-    // Start is called before the first frame update
     void Awake()
     {
-        foreach (var item in textObjects)
+        if(textObjects == null)
+            textObjects = GameObject.FindGameObjectsWithTag("TextObject");
+        
+        
+        foreach (GameObject item in textObjects)
         {
             item.SetActive(false);
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    }    
 }
