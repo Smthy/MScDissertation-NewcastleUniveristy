891d2c2025f0a5c6c03f78a600ac567f47e6f502
Alex Smith <alexsmithgamedev@gmail.com>
2022-06-27 16:05:59 +0100

    Fixed Teleportation
    Creating a fade to black and back to clear to reduce the chance of motion sickness

---
+++ BreweryTourVR/Assets/Scripts/LocomotionController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class LocomotionController : MonoBehaviour
+{
+    public XRController leftRay, rightRay;
+    public InputHelpers.Button teleportActivator;
+    public float activationThresold = 0.1f;
+
+    void Update()
+    {
+        if (leftRay)
+        {
+            leftRay.gameObject.SetActive(CheckIfActivated(leftRay));
+        }
+
+        if (rightRay)
+        {
+            rightRay.gameObject.SetActive(CheckIfActivated(leftRay));
+        }
+    }
+
+    public bool CheckIfActivated(XRController controller)
+    {
+        InputHelpers.IsPressed(controller.inputDevice, teleportActivator, out bool isActivated, activationThresold);
+        return isActivated;
+    }
+}
