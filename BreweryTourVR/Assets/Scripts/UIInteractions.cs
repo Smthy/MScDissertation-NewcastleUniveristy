f769c650f131ab2b62a265738ffa95afe0a722ff
Alex Smith <alexsmithgamedev@gmail.com>
2022-08-09 18:03:01 +0100

    UI working
    Just need to work on the fade system then its finished

--- BreweryTourVR/Assets/Scripts/UIInteractions.cs
+++ BreweryTourVR/Assets/Scripts/UIInteractions.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Animations;
 
 public class UIInteractions : MonoBehaviour
 {
-    public GameObject starterText;
-
-    public GameObject settingLayer, teleportLayer, mainMenuLayer;
+    public GameObject starterText, settingLayer, teleportLayer, mainMenuLayer;
+    public CharacterController userController;
+    public GameObject user;
+    public Transform[] teleportTransforms;
+    
 
     private void Start()
     {
@@ -16,11 +19,9 @@ public class UIInteractions : MonoBehaviour
         mainMenuLayer.SetActive(true);
     }
 
-
     public void Close()
     {
         starterText.SetActive(false);
-
     }
 
     public void TeleportationMenu()
@@ -42,5 +43,74 @@ public class UIInteractions : MonoBehaviour
         settingLayer.SetActive(false);
         teleportLayer.SetActive(false);
         mainMenuLayer.SetActive(true);
+    }
+
+    /*
+     * play fade in
+     * teleport to location
+     * play fade out
+    */ 
+
+    public void KettleTeleportation()
+    {
+        userController.enabled = false;
+        user.transform.position = teleportTransforms[0].transform.position;
+        userController.enabled = true;
+    }
+
+    public void MasherTeleportation()
+    {
+        userController.enabled = false;
+        user.transform.position = teleportTransforms[1].transform.position;
+        userController.enabled = true;
+    }
+
+    public void FermTeleportation()
+    {
+        userController.enabled = false;
+        user.transform.position = teleportTransforms[2].transform.position;
+        userController.enabled = true;
+    }
+
+    public void CoolingTeleportation()
+    {
+        userController.enabled = false;
+        user.transform.position = teleportTransforms[3].transform.position;
+        userController.enabled = true;
+    }
+
+    public void PressurisedTeleportation()
+    {
+        userController.enabled = false;
+        user.transform.position = teleportTransforms[4].transform.position;
+        userController.enabled = true;
+    }
+
+    public void Liter800Teleportation()
+    {
+        userController.enabled = false;
+        user.transform.position = teleportTransforms[5].transform.position;
+        userController.enabled = true;
+    }
+
+    public void coolingPanelTeleportation()
+    {
+        userController.enabled = false;
+        user.transform.position = teleportTransforms[6].transform.position;
+        userController.enabled = true;
+    }
+
+    public void liter400Teleportation()
+    {
+        userController.enabled = false;
+        user.transform.position = teleportTransforms[7].transform.position;
+        userController.enabled = true;
+    }
+
+    public void MiddleTeleportation()
+    {
+        userController.enabled = false;
+        user.transform.position = teleportTransforms[8].transform.position;
+        userController.enabled = true;
     }
 }
